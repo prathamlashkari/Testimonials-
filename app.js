4c29f686724a8c225f12122b83d624fbca2ada06
nvineng <naveen211063cse@sdbc.ac.in>
2024-01-07 16:13:56 +0530

    initailly javascript part

---
+++ app.js
@@ -0,0 +1,20 @@
+(function () {
+    const customerImage = document.querySelector('#customer-img')
+    const customerName = document.querySelector('#customer-name')
+    const customerText = document.querySelector('#customer-text')
+
+
+
+    const but = document.querySelector('.btn')
+
+    let index = 0
+    const customers = []
+
+    function Customer(img, name, text) {
+        this.img = img
+        this.name = name
+        this.text = text
+
+    }
+
+})
