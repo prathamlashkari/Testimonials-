dd0caabfbb604b0b9880c6397a8455c9c8c612fe
nvineng <naveen211063cse@sdbc.ac.in>
2024-01-07 17:46:14 +0530

    javascript

--- app.js
+++ app.js
@@ -1,12 +1,9 @@
 (function () {
+
     const customerImage = document.querySelector('#customer-img')
     const customerName = document.querySelector('#customer-name')
     const customerText = document.querySelector('#customer-text')
-
-
-
-    const but = document.querySelector('.btn')
-
+    const btn = document.querySelector('.btn')
     let index = 0
     const customers = []
 
@@ -17,4 +14,47 @@
 
     }
 
+    function createCustomer(img, name, text) {
+        let Img = `./img/${img}.webp`
+        let customer = new Customer(Img, name, text)
+
+        customers.push(customer)
+
+    }
+
+
+    createCustomer(0, 'Natesh', 'Hello My dear Lorem, ipsum dolor sit amet consectetur adipisicing elit. Impedit minus animi eaque quos saepe obcaecati porro necessitatibus praesentium repudiandae debitis perferendis quibusdam exercitationem facilis, accusamus, qui amet nulla veritatis maiores! Vitae, optio sequi dolore reiciendis at sit pariatur nihil debitis aut delectus corporis voluptate, facilis, modi aperiam! Sunt iure, itaque quas deleniti hic rem laboriosam')
+    createCustomer(1, 'Khushi', 'Hello My dear Lorem, ipsum dolor sit amet consectetur adipisicing elit. Impedit minus animi eaque quos saepe obcaecati porro necessitatibus praesentium repudiandae debitis perferendis quibusdam exercitationem facilis, accusamus, qui amet nulla veritatis maiores! Vitae, optio sequi dolore reiciendis at sit pariatur nihil debitis aut delectus corporis voluptate, facilis, modi aperiam! Sunt iure, itaque quas deleniti hic rem laboriosam')
+    createCustomer(2, 'Panda', 'Hello My dear Lorem, ipsum dolor sit amet consectetur adipisicing elit. Impedit minus animi eaque quos saepe obcaecati porro necessitatibus praesentium repudiandae debitis perferendis quibusdam exercitationem facilis, accusamus, qui amet nulla veritatis maiores! Vitae, optio sequi dolore reiciendis at sit pariatur nihil debitis aut delectus corporis voluptate, facilis, modi aperiam! Sunt iure, itaque quas deleniti hic rem laboriosam')
+    createCustomer(3, 'Naveen', 'Hello My dear Lorem, ipsum dolor sit amet consectetur adipisicing elit. Impedit minus animi eaque quos saepe obcaecati porro necessitatibus praesentium repudiandae debitis perferendis quibusdam exercitationem facilis, accusamus, qui amet nulla veritatis maiores! Vitae, optio sequi dolore reiciendis at sit pariatur nihil debitis aut delectus corporis voluptate, facilis, modi aperiam! Sunt iure, itaque quas deleniti hic rem laboriosam')
+    createCustomer(4, 'Vedika', 'Hello My dear Lorem, ipsum dolor sit amet consectetur adipisicing elit. Impedit minus animi eaque quos saepe obcaecati porro necessitatibus praesentium repudiandae debitis perferendis quibusdam exercitationem facilis, accusamus, qui amet nulla veritatis maiores! Vitae, optio sequi dolore reiciendis at sit pariatur nihil debitis aut delectus corporis voluptate, facilis, modi aperiam! Sunt iure, itaque quas deleniti hic rem laboriosam')
+
+
+    btn.forEach(function (button) {
+
+        button.addEventListener('click', function (e) {
+            if (e.target.parentElement.classList.contains('preBtn')) {
+                if (index === 0) {
+                    index = customers.length
+                }
+                index--
+                customerImage.src = customers[index].img
+                customerName.textContest = customers[index].name
+                customerText.textContent = customers[index].text
+            }
+
+            if (e.target.parentElement.classList.contains('nextBtn')) {
+                index++
+                if (index === customers.length) {
+                    index = 0
+                }
+
+                customerImage.src = customers[index].img
+                customerName.textContest = customers[index].name
+                customerText.textContent = customers[index].text
+            }
+
+        })
+
+    })
 })
